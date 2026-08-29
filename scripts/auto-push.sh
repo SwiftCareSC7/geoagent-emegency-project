f0d4836acbcf169e9a83756306605444bd180160
Priyanshu Yadav <spec.priyanshu@gmail.com>
2026-08-30 00:58:18 +0530

    chore: remove auto-push script and log

--- scripts/auto-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Background push loop: pushes new commits to origin every 60 seconds
-cd /Users/priyanshu/Documents/geoagent-emegency-project
-while true; do
-  sleep 60
-  git push origin main 2>&1 | tail -3
-done
